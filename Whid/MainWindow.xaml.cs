44230fdfd289bc1105b7a766ec3c6e281b3047ad
raphael-kahler <raphael.kahler@gmail.com>
2019-05-18 00:03:13 -0700

    Add WPF app as frontend

---
+++ Whid/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Whid.Framework;
+
+namespace Whid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel _viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = _viewModel = new MainWindowViewModel(new InMemorySummaryService());
+        }
+    }
+}
---
+++ Whid/MainWindowViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Whid.Domain;
+using Whid.Framework;
+
+namespace Whid
+{
+    internal class MainWindowViewModel
+    {
+        public IEnumerable<Summary> Summaries { get; set; }
+
+        public MainWindowViewModel(ISummaryService service)
+        {
+            Summaries = service.GetSummaries();
+        }
+    }
+}
